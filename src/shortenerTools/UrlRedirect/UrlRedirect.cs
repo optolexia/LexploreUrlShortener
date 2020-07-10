c7839bceff492047e175abf8ba06f119a80a31da
Erik Alsmyr <erik.alsmyr@lexplore.com>
2020-07-10 15:49:45 +0200

    Hard code referrer

--- src/shortenerTools/UrlRedirect/UrlRedirect.cs
+++ src/shortenerTools/UrlRedirect/UrlRedirect.cs
@@ -44,7 +44,7 @@ namespace Cloud5mins.Function
                     log.LogInformation($"Found it: {newUrl.Url}");
                     newUrl.Clicks++;
                     var newClickStats = new ClickStatsEntity(newUrl.RowKey);
-                    newClickStats.Referrer = req.Headers.Referrer.ToString();
+                    newClickStats.Referrer = "https://www.lexplore.com";
                     stgHelper.SaveClickStatsEntity(newClickStats);
                     await stgHelper.SaveShortUrlEntity(newUrl);
                     redirectUrl = WebUtility.UrlDecode(newUrl.Url);
